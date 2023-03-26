990528a3b82c65ce580053c9fbd2ed7b4f24a2df
SorahsNobody <benbettencourt@u.boisestate.edu>
2023-03-26 10:55:16 -0600

    added Akshharaa's start page

--- SET/src/app/app-routing.module.ts
+++ SET/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { QCreateComponent } from './q-create/q-create.component';
+import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
   {path: 'categories', component: CategoryComponent},
-  {path: 'queryCreate', component: QCreateComponent}
-  //{path: 'start', component: AppComponent}
+  {path: 'queryCreate', component: QCreateComponent},
+  {path: 'start', component: StartComponent}
 ];
 
 @NgModule({
--- SET/src/app/app.module.ts
+++ SET/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QCreateComponent } from './q-create/q-create.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoryComponent,
-    QCreateComponent
+    QCreateComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
