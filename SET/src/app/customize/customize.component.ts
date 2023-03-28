2660f6336b1403d9d43bdc4b03f501f273431a32
mswendell1 <110133210+mswendell1@users.noreply.github.com>
2023-03-28 13:22:54 -0600

    Added working buttons and images

--- SET/src/app/customize/customize.component.ts
+++ SET/src/app/customize/customize.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customize',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./customize.component.css']
 })
 export class CustomizeComponent {
+  constructor(private router: Router) { }
 
+  toResult(): void {
+    this.router.navigateByUrl("/results");
+  }
 }
