660c171dbfdffd37c249197fa6623c091ed7a3ed
Ben Bettencourt <BENBETTENCOURT@boisestate.edu>
2023-03-28 14:36:23 -0600

    added auto navigation to start screen

--- SET/src/app/app.component.ts
+++ SET/src/app/app.component.ts
@@ -9,8 +9,7 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
   constructor(private route: Router) { }
   ngOnInit(): void {
-    //this.route.navigateByUrl("/start");
+    this.route.navigateByUrl("/start");
   }
   title = 'SET';
-  //playerName = 'Ben'; // Set player name for the game TODO: will need to change dynamically
 }
